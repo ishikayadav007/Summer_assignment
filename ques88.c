5c1ca500d84dae25894baf4aea6b94b686769e9e
ishikayadav007 <ishikayadav6002@gmail.com>
2026-06-26 18:08:16 +0530

    Delete ques88.c

--- ques88.c
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-int main(){char s[200];fgets(s,200,stdin);int l=0,r=strlen(s)-2,p=1;while(l<r){if(s[l++]!=s[r--]){p=0;break;}}printf(p?"Palindrome":"Not Palindrome");}
