4a06e836ae452cbc968e9e303535f4362dbbd879
ishikayadav007 <ishikayadav6002@gmail.com>
2026-06-26 18:14:32 +0530

    Delete ques84.c

--- ques84.c
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-#include <ctype.h>
-int main(){char s[200];int v=0,c=0;fgets(s,200,stdin);for(int i=0;s[i];i++){char ch=tolower(s[i]);if(ch>='a'&&ch<='z'){if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')v++;else c++;}}printf("Vowels=%d Consonants=%d",v,c);}
