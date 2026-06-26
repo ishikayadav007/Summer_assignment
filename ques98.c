a3320837cb21e2d0d9b43806ebfb6159f74cef62
ishikayadav007 <ishikayadav6002@gmail.com>
2026-06-26 18:16:46 +0530

    Delete ques98.c

--- ques98.c
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-int main(){char s[200];fgets(s,200,stdin);int d=0,l=0;for(int i=0;s[i];i++){if(s[i]>='0'&&s[i]<='9')d++;if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z'))l++;}printf("Letters=%d Digits=%d",l,d);}
