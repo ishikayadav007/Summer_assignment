b9ba527b75fafb2f0c7482c3507fc77d650f3da6
ishikayadav007 <ishikayadav6002@gmail.com>
2026-06-27 22:40:49 +0530

    Add files via upload

---
+++ Day27/ques107/salary_management.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int main(){string name;double basic;cin>>name>>basic;double hra=0.2*basic,da=0.1*basic;cout<<"Employee: "<<name<<"\nNet Salary: "<<basic+hra+da;}
---
+++ Day27/ques108/marksheet_generation.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int main(){string name;int m1,m2,m3;cin>>name>>m1>>m2>>m3;int total=m1+m2+m3;double per=total/3.0;cout<<"Name: "<<name<<"\nTotal: "<<total<<"\nPercentage: "<<per<<"%";}
